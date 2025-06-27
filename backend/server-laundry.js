136a79427b53866a50985d53b619aa6ae2ea4d3a
ishantawar <163636877+ishantawar@users.noreply.github.com>
2025-06-27 05:48:52 +0530

    Update server-laundry.js

--- backend/server-laundry.js
+++ backend/server-laundry.js
@@ -17,6 +17,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
   "http://localhost:8080",
+  "https://cleancarepro.vercel.app",
   "https://cleancare-pro-api.onrender.com",
   "https://cleancare-pro-frontend.onrender.com",
 ];
